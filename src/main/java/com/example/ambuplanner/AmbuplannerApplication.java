29427096ee6f5e30f6ae8ac56d7fe83ea8077e39
Alberto Rico Garcia <albrigar93@gmail.com>
2017-09-22 18:00:18 +0200

    Proyecto Inicial Spring-boot

---
+++ src/main/java/com/example/ambuplanner/AmbuplannerApplication.java
@@ -0,0 +1,12 @@
+package com.example.ambuplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmbuplannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmbuplannerApplication.class, args);
+	}
+}
