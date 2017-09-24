96e6dc979960c30c23e33195bd1fe82b30b454d8
Alberto Rico Garcia <albrigar93@gmail.com>
2017-09-24 09:46:42 +0200

    Enable CorsMapping Allow-Origin

--- src/main/java/com/example/ambuplanner/controllers/AmbuPlannerController.java
+++ src/main/java/com/example/ambuplanner/controllers/AmbuPlannerController.java
@@ -7,6 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +20,7 @@ import java.util.List;
 public class AmbuPlannerController {
 
     @RequestMapping(method = RequestMethod.GET)
+    @CrossOrigin
     public ResponseEntity index() {
         AppLaunch initApp = new AppLaunch();
         initApp.launch();
